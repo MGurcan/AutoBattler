6f68710e312738690e0fbf2832acd53881a405dc
Mehmet Gürcan <78200658+MGurcan@users.noreply.github.com>
2023-10-17 23:40:42 +0300

    feat: shop initial implementation, instantiate newly purchased character, 3d character in canvas

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -22,10 +22,10 @@ public class Shop : MonoBehaviour
 	[System.Serializable] public class ShopItem
 	{
 		public GameObject prefab;
-		public int Price;
+		public int price;
 		public int shopItemId;
         public string name;
-        public string ToString => "Item Price: " + Price + " Has " + name + " of them.";
+        public string ToString => "Item Price: " + price + " Has " + name + " of them.";
     }
 
 	public List<ShopItem> ShopItemsList;
@@ -34,9 +34,12 @@ public class Shop : MonoBehaviour
 
     public Button[] buyButtons;
 
+    public GameObject ShopPanel;
+    public GameObject purchaseButtonPrefab;
+
+
     public Character PurchaseCharacter(int itemIndex)
     {
-        Debug.Log(itemIndex);
         Character character = new Character(ShopItemsList[itemIndex].shopItemId, ShopItemsList[itemIndex].name);
         gameManager.characterList.Add(character);
 
@@ -45,10 +48,25 @@ public class Shop : MonoBehaviour
     }
 
     private void Start() {
-        int len = buyButtons.Length;
+        int len = ShopItemsList.Count;
+        Transform shopItemsTransform = ShopPanel.transform.Find("ShopItems");
+
 		for (int i = 0; i < len; i++) {
+            GameObject purchaseButton = Instantiate(purchaseButtonPrefab, shopItemsTransform);
+            Button button = purchaseButton.GetComponent<Button>();
+
+            button.transform.Find("NameText").GetComponent<Text>().text = ShopItemsList[i].name;
+            button.transform.Find("PriceText").GetComponent<Text>().text = ShopItemsList[i].price.ToString() + "$";
+
+
+            Transform buttonGameObjectTransform = button.transform.Find("ButtonGameObject");
+            GameObject buttonGameObject = Instantiate(ShopItemsList[i].prefab, buttonGameObjectTransform);
+            buttonGameObject.transform.localScale = new Vector3(80f, 80f, 80f);
+            buttonGameObject.layer = LayerMask.NameToLayer("UI");
+            Destroy(buttonGameObject.GetComponent<DragObject>());
+
             int itemIndex = i;
-			buyButtons[i].onClick.AddListener(() => PurchaseCharacter(itemIndex));
+			button.onClick.AddListener(() => PurchaseCharacter(itemIndex));
 		}
     }
 }
